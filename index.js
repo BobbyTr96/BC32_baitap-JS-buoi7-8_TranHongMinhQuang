fe6de1e16cd57fb7eed3d6c6e1be9571a576cf49
BobbyTr96 <pmap58@gmail.com>
2022-08-06 11:58:51 +0700

    add1

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ function themSo() {
   // tạo biến số để lấy giá trị của người dùng , sau nó dùng 'push' để thêm vào mảng numbers
   let so = document.getElementById("num").value * 1;
   numbers.push(so);
-    
+
   // đầu ra
-  document.getElementById("themSo").innerHTML = numbers  ;
+  document.getElementById("themSo").innerHTML = numbers;
 }
 
 //Bài 1 :tính tổng số dương
@@ -73,13 +73,13 @@ function timSoDuongNhoNhat() {
   //tạo 1 mảng để tìm số dương nhỏ nhất
   let soDuongNhoNhat = "";
   // dùng vòng lặp để kiếm tra và chỉ lấy số dương đưa vào mảng mới 'soDuong'
-  
+
   for (let a = 0; a < numbers.length; a++) {
     if (numbers[a] > 0) {
       soDuong.push(numbers[a]);
     }
-   
-     if (soDuong.length > 0) {
+
+    if (soDuong.length > 0) {
       soDuongNhoNhat = soDuong[0];
       for (let i = 0; i < soDuong.length; i++) {
         if (soDuong[i] < soDuongNhoNhat) {
@@ -88,9 +88,9 @@ function timSoDuongNhoNhat() {
       }
     }
     if (soDuong.length === 0) {
-        document.getElementById("soDuongNhoNhat").innerHTML = "Không có số dương"
-        return
-    } 
+      document.getElementById("soDuongNhoNhat").innerHTML = "Không có số dương";
+      return;
+    }
   }
 
   // đầu ra
@@ -99,33 +99,115 @@ function timSoDuongNhoNhat() {
 
 //Bài 5 : Tìm số chẵn cuối cùng
 function timSoChanCuoiCung() {
-    // tạo 1 biến để chưa số chẵn
-    let soChan = "" ;
+  // tạo 1 biến để chưa số chẵn
+  let soChan = "";
 
-    // duyệt mảng tìm số chẵn , nếu là số chẵn thì gán ngược lại cho biến 'soChan' , ta duyệt từ index 0 trở đi 
-    for(let i = 0 ; i < numbers.length; i++) {
-        if(numbers[i] % 2 === 0) {
-            soChan = numbers[i]
-        }
+  // duyệt mảng tìm số chẵn , nếu là số chẵn thì gán ngược lại cho biến 'soChan' , ta duyệt từ index 0 trở đi
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2 === 0) {
+      soChan = numbers[i];
     }
+  }
 
-    /// đầu ra
-    document.getElementById("soChanCuoiCung").innerHTML = soChan;
+  /// đầu ra
+  document.getElementById("soChanCuoiCung").innerHTML = soChan;
 }
 
 // bài 6 ; đổi chỗ
 function doiCho() {
-    // Dom
-    var num1 =document.getElementById("num1").value*1 ;
-    var num2 =document.getElementById("num2").value*1 ;
+  // Dom
+  var num1 = document.getElementById("num1").value * 1;
+  var num2 = document.getElementById("num2").value * 1;
 
-    // xử lý
-// tạo 1 biến chứa tạm giá trị của mảng vị trí num1
-let place1 = "" ;
-place1 = numbers[num1]
-numbers[num1] = numbers[num2]
-numbers[num2] = place1
+  // xử lý
+  // tạo 1 biến chứa tạm giá trị của mảng vị trí num1
+  let place1 = "";
+  place1 = numbers[num1];
+  numbers[num1] = numbers[num2];
+  numbers[num2] = place1;
 
-// đầu ra
-document.getElementById("doiCho").innerHTML = numbers
+  // đầu ra
+  document.getElementById("doiCho").innerHTML = numbers;
 }
+
+//Bài 7 : sắp xếp tăng dần
+function tangDan() {
+  // dùng hàm 'sort' đưa vào giá trị (a,b) => điều kiện a - b  là nếu a - b mà > 0 thì a lớn hơn b , ngược lại , và hàm tự sắp xếp lại theo yêu cầu
+  // (a-b) là sắp xếp từ nhỏ dến lớn
+  // (b - a) là sắp xếp từ lớn đến nhỏ
+  numbers.sort((a, b) => a - b);
+
+  // đầu ra
+  document.getElementById("sapXep").innerHTML = numbers;
+}
+
+// bài 8 : tìm số nguyên tố đầu tiên có  trong mảng mà ng dùng nhập vào
+function timSnt() {
+  // B1 tạo 1 func kiểm tra xem đó có phải là số nguyên tố hay ko
+
+  function kiemTraSnt(n) {
+    // tham số "n" = numbers[i]
+    // tạo cờ hiệu flag = true thì là số nt , flag =  false thì ko phải snt
+flag = true
+    // loại bỏ số ko phải snt , số < 2 ko phải snt , nếu ko phải thì sẽ trả ra  giá trị flag = false
+    if (n < 2) return (flag = false);
+    
+
+    for (let i = 2; i < n; i++) {
+      if (n % i === 0) {
+        flag = false;
+        break;
+      }
+    }
+    return flag
+  }
+
+  // duyệt mảng để xét từng con số xem nó có phải snt hay ko
+  for ( let o = 0 ; o < numbers.length ; o++){
+    if(kiemTraSnt(numbers[o])) {
+      document.getElementById("timSo").innerHTML = numbers[o]
+      break
+    }
+  }
+}
+
+//Bài 9 : kiểm tra số nguyên dùng toán tử Number.isInteger , số nguyên là bao gồm số nguyên âm và số nguyên dương vd : 5 ,  -1 // vd : 5.5 ko phải là số nguyên
+  function demSoNguyen(){
+    //tạo 1 biến để đếm có bao nhiêu số nguyên trong mảng
+    let soNguyen = 0 ;
+    // duyệt mảng
+    for( let i = 0 ; i < numbers.length ; i++) {
+      if (Number.isInteger(numbers[i])) {
+        soNguyen++
+      }
+    }
+
+    // đầu ra
+    document.getElementById("demSo").innerHTML = soNguyen
+  }
+
+
+  // bài 10 : đếm số âm và số dương sau đó so sánh
+  function soSanh() {
+    // tạo 2 biến , 1 cái để đếm số lượng số âm ,  1 cái để đếm số lượng số dương
+    let soAm = 0 ;
+    let soDuong = 0 ;
+
+    // duyệt mảng bằng vòng lặp 'for' để duyệt từng giá trị trong mảng numbers và kiểm tra xem nó là số âm hay dương , nếu âm thì biến soAm++ , nếu là số dương thì biến  soDuong++
+    for( let i=0; i<numbers.length ; i++) {
+      if(numbers[i] > 0) {
+        soDuong++
+      }else if (numbers[i] < 0) {
+        soAm++
+      }
+    }
+
+    if(soDuong === soAm ) {
+      document.getElementById("soSanh").innerHTML = "Số Dương " + "="  + " Số âm" ;
+    }else if (soDuong > soAm ) {
+      document.getElementById("soSanh").innerHTML = "Số Dương " + ">"  + " Số âm" ;
+    }else if (soDuong < soAm ) {
+      document.getElementById("soSanh").innerHTML = "Số Dương " + "<"  + " Số âm" ;
+    }
+  }
+
